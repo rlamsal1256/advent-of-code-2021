5186ebab4419c0645c85c4921350b4fd34e1ff6a
Rupak Lamsal <rupak@mojotech.com>
2021-12-02 10:09:27 -0500

    take 2

--- solutions/day02.rb
+++ solutions/day02.rb
@@ -22,5 +22,4 @@ end
 
 input = File.read("day02_input.txt").lines.map(&:split)
 p input.inject([0 ,0]){ |(x, y), (dir, val)| new_pos_part_1(x, y, dir, val.to_i) }.inject(:*)
-(pos, depth, _) = input.inject([0 ,0, 0]){ |(x, y, a), (dir, val)| new_pos_part_2(x, y, a, dir, val.to_i) }
-p pos*depth
+p input.inject([0 ,0, 0]){ |(x, y, a), (dir, val)| new_pos_part_2(x, y, a, dir, val.to_i) }.take(2).inject(:*)
