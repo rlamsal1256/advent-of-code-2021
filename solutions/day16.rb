215aa1c479eeb8a239110b2532cf361cf8493089
Rupak Lamsal <rupak@mojotech.com>
2021-12-17 16:14:25 -0500

    day 16 part 2 solutions

--- solutions/day16.rb
+++ solutions/day16.rb
@@ -6,21 +6,40 @@ end
 
 def decode(packet)
   $v += get(packet, 3)
-  type_id = get(packet, 3)
+  type = get(packet, 3)
 
-  if type_id == 4
-    packet.shift(4) while packet.shift == "1"
-    packet.shift(4)
+  if type == 4
+    val = []
+    val << packet.shift(4) while packet.shift == "1"
+    val << packet.shift(4)
+    [type, val.flatten.join.to_i(2)]
   else
     if packet.shift == "0"
-      sub_packet = packet.shift(get(packet, 15))
-      decode(sub_packet) until sub_packet.empty?
+      sub_packets = []
+      packets = packet.shift(get(packet, 15))
+      sub_packets << decode(packets) until packets.empty?
     else
-      get(packet, 11).times{ decode(packet) }
+      sub_packets = get(packet, 11).times.map{ decode(packet) }
     end
+    [type, sub_packets]
   end
 end
 
 input = File.read("day16_input.txt").chomp.chars.map{ |c| c.hex.to_s(2).rjust(4, "0").chars}.flatten
-decode(input)
+packet = decode(input)
 p $v
+
+def eval(packet)
+  case packet.first
+  when 0; packet.last.map(&method(:eval)).reduce(:+)
+  when 1; packet.last.map(&method(:eval)).reduce(:*)
+  when 2; packet.last.map(&method(:eval)).min
+  when 3; packet.last.map(&method(:eval)).max
+  when 4; packet.last
+  when 5; packet.last.map(&method(:eval)).reduce(:>) ? 1 : 0
+  when 6; packet.last.map(&method(:eval)).reduce(:<) ? 1 : 0
+  when 7; packet.last.map(&method(:eval)).reduce(:==) ? 1 : 0
+  end
+end
+
+p eval(packet)
